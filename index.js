3a500d7656e190943d7164cc5a36f686643390a0
devoloper-rakib <rakibhasansohag133@gmail.com>
2022-08-28 21:00:14 +0600

    upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const chart = document.querySelector('#chart').getContext('2d');
+// create a new chart instance
+new Chart(chart, {
+	type: 'line',
+	data: {
+		labels: [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec',
+		],
+		datasets: [
+			{
+				label: 'BTC',
+				data: [
+					29741, 35157, 15454, 54454, 21584, 24524, 24542, 24654, 24564, 45445,
+					44654, 55011,
+				],
+				borderColor: 'red',
+				borderWidth: 2,
+			},
+			{
+				label: 'ETH',
+				data: [
+					54545, 12424, 12542, 32242, 13124, 32313, 21245, 13421, 002114, 12454,
+					44654, 50512,
+				],
+				borderColor: 'blue',
+				borderWidth: 2,
+			},
+		],
+	},
+	options: {
+		responsive: true,
+	},
+});
+
+// show or hide sidebar section start
+
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sideBar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+	sideBar.style.display = ' block';
+});
+
+closeBtn.addEventListener('click', () => {
+	sideBar.style.display = 'none';
+});
+
+// show or hide sidebar section end
